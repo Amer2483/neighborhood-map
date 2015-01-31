598512b6073eccb8aad7951f9efa54f0ab00d520
Subrat Rout <subratnrout@gmail.com>
2015-01-31 13:52:49 -0800

    Added map markers identifyng locations

--- js/main.js
+++ js/main.js
@@ -1,21 +1,34 @@
-function initialize() {
-        var mapOptions = {
-          center: { lat: 32.73, lng: -117.162},
-          zoom: 12
+function point(name, lat, lng){
+  this.name = name;
+  this.lat = ko.observable(lat);
+  this.lng = ko.observable(lng);
+
+  var myLatlng = new google.maps.LatLng(lat, lng);
+
+  var marker = new google.maps.Marker({
+    position: myLatlng,
+    map: map,
+    title: name,
+    draggable: true
+  });
+}
+
+
+
+var mapOptions = {
+          center: new google.maps.LatLng(32.73,-117.162),
+          zoom: 12,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
         };
-        var map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
-}
 
-google.maps.event.addDomListener(window, 'load', initialize);
+var map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
 
-// For asynchronous loading of maps
-function loadScript() {
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&' +
-      'callback=initialize';
-  document.body.appendChild(script);
+
+var viewModel = {
+  points: ko.observableArray([
+    new point('Place1', 32.74, -117),
+    new point('Place2', 32.72, -117)
+    ])
 }
-window.onload = loadScript;
 
 ko.applyBindings(viewModel);
